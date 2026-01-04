7488be523ecca6d4674acb713e7550b2e82b7688
Lars Magnusson <lars.v.magnusson@hiof.no>
2026-01-04 16:03:36 +0100

    added some fixes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,6 +27,16 @@ return {
         })
     end
   },
+  { "ms-jpq/coq_nvim", branch = "coq" },
+  { 
+    "neovim/nvim-lspconfig",
+    config = function()
+      local coq = require("coq")
+      -- require('lspconfig').pyright.setup(coq.lsp_ensure_capabilities({}))
+      vim.lsp.config('pyright', coq.lsp_ensure_capabilities({}))
+			vim.lsp.enable({'pyright'})
+    end 
+  },
   { 
     "williamboman/mason.nvim",
     config = function()
@@ -38,14 +48,6 @@ return {
     opts = { ensure_installed = { "pyright" } },
     config = function(_, opts)
       require("mason-lspconfig").setup(opts)  
-    end 
-  },
-  { "ms-jpq/coq_nvim", branch = "coq" },
-  { 
-    "neovim/nvim-lspconfig",
-    config = function()
-      local coq = require("coq")
-      require("lspconfig").pyright.setup(coq.lsp_ensure_capabilities({}))
     end 
   },
   { 
@@ -82,45 +84,45 @@ return {
     "sourcegraph/sg.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   },
-  {
-    'lavima/image.nvim',
-    config = function()
-      -- default config
-      require("image").setup({
-        backend = "kitty",
-        integrations = {
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-          },
-          neorg = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "norg" },
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-      })
-    end
-  },
-  {
-    'lavima/neonotebook.nvim',
-		dependencies = { "lavima/image.nvim" },
-    config = function()
-      require('neonotebook').setup({})
-    end
-  },
+  -- {
+  --   'lavima/image.nvim',
+  --   config = function()
+  --     -- default config
+  --     require("image").setup({
+  --       backend = "kitty",
+  --       integrations = {
+  --         markdown = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           download_remote_images = true,
+  --           only_render_image_at_cursor = false,
+  --           filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  --         },
+  --         neorg = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           download_remote_images = true,
+  --           only_render_image_at_cursor = false,
+  --           filetypes = { "norg" },
+  --         },
+  --       },
+  --       max_width = nil,
+  --       max_height = nil,
+  --       max_width_window_percentage = nil,
+  --       max_height_window_percentage = 50,
+  --       window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+  --       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --       editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+  --       tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  --       hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+  --     })
+  --   end
+  -- },
+  -- {
+  --   'lavima/neonotebook.nvim',
+  -- dependencies = { "lavima/image.nvim" },
+  --   config = function()
+  --     require('neonotebook').setup({})
+  --   end
+  -- },
 }
